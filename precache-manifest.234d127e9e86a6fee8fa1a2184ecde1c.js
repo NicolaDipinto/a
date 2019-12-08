7eb6b6fad8bd3b9089ef720cfbdec008772291a7
nicola_dipinto <dipinto.nicola.dev@gmail.com>
2019-12-08 23:44:32 +0100

    Updates

---
+++ precache-manifest.234d127e9e86a6fee8fa1a2184ecde1c.js
@@ -0,0 +1,26 @@
+self.__precacheManifest = (self.__precacheManifest || []).concat([
+  {
+    "revision": "ba81465cf96026e6d25e0b5f994761e9",
+    "url": "/a/index.html"
+  },
+  {
+    "revision": "2cb4b36bd6f5f2b231b6",
+    "url": "/a/static/css/main.d1b05096.chunk.css"
+  },
+  {
+    "revision": "12ed7e1fae371b4cb936",
+    "url": "/a/static/js/2.e494a0c8.chunk.js"
+  },
+  {
+    "revision": "d705cb622423d72c5defbf368ca70dcc",
+    "url": "/a/static/js/2.e494a0c8.chunk.js.LICENSE"
+  },
+  {
+    "revision": "2cb4b36bd6f5f2b231b6",
+    "url": "/a/static/js/main.7e9674c3.chunk.js"
+  },
+  {
+    "revision": "ab36ecc9debe8b7b49b4",
+    "url": "/a/static/js/runtime-main.bb05e233.js"
+  }
+]);
